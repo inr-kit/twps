2e8634453b955289238ebc1b1983cee62b821a8d
anton.travleev <anton.travleev@gmail.com>
2020-01-20 22:22:07 +0100

    After publishing on PyPi

--- setup.py
+++ setup.py
@@ -1,18 +1,34 @@
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 import twps
 
 setup(name='twps',
+      packages=['twps', ],
+
       # version: X.Y.Z, where:
-      #    X -- major version. Different major versions are not back-compatible.
+      #    X -- major version. Major versions are not back-compatible.
       #         New major version number, when code is rewritten
       #
       #    Y -- minor version. New minor version, when new function(s) added.
       #
       #    Z -- update, new update number when a bug is fixed.
       version=twps.__version__,  # '1.3.2',
-      description='Text-with-Snippets Preprocessor',
+      license='GPL',
+
+      description='TWPS: (T)ext (W)ith (P)ython (S)nippets preprocessor',
       author='A.Travleev',
-      author_email='anton.travleev@kit.edu',
-      packages=['twps', ],
-      scripts = ['twps/ppp.py']
+      author_email='anton.travleev@gmail.com',
+      url='https://github.com/inr-kit/twps',
+      download_url='https://github.com/inr-kit/twps/archive/2.0.0.tar.gz',
+      keywords='TEXT PYTHON SNIPPETS TEMPLATE PREPROCESSOR'.split(),
+      scripts=['twps/ppp.py'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'Intended Audience :: Education',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 2.7'
+      ],
+      install_requires='re textwrap traceback stat datetime'.split(),
       )
